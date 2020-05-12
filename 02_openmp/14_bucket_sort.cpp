1579d5a1844f91123d9cc2c40db50540d5226526
aska-0096 <65143451+aska-0096@users.noreply.github.com>
2020-05-12 23:20:41 +0900

    Update 14_bucket_sort.cpp

--- 02_openmp/14_bucket_sort.cpp
+++ 02_openmp/14_bucket_sort.cpp
@@ -17,15 +17,11 @@ int main() {
   for (int i=0; i<range; i++) {
     bucket[i] = 0;
   }
-for (int a=0; a<range; a++){
-  int j=0;
-#pragma omp parallel for reduction(+:j)
-  for (int i=0; i<n; i++) {
-    if(key[i] == a)
-      j+=1;
+#pragma omp parallel for
+  for(int i=0; i<n; i++){
+    #pragma omp atomic update
+    bucket[key[i]]++;
   }
-  bucket[a]=j;
-}
   for (int i=0, j=0; i<range; i++) {
     for (; bucket[i]>0; bucket[i]--) {
       key[j++] = i;
