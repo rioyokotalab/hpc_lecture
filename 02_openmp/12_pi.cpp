d7732652c5fe0331c3085904d5c8aec2f58fa79e
Jiang-Ji-Wei <82709441+Jiang-Ji-Wei@users.noreply.github.com>
2021-04-22 00:54:17 +0800

    Update 12_pi.cpp

--- 02_openmp/12_pi.cpp
+++ 02_openmp/12_pi.cpp
@@ -1,12 +1,16 @@
 #include <cstdio>
+#include<omp.h>
 
 int main() {
   int n = 10;
   double dx = 1. / n;
   double pi = 0;
+  #pragma omp parallel for reduction(+:pi)
   for (int i=0; i<n; i++) {
     double x = (i + 0.5) * dx;
+    //printf("%d",omp_get_thread_num());
     pi += 4.0 / (1.0 + x * x) * dx;
+    //printf("%d\n",omp_get_thread_num());
   }
   printf("%17.15f\n",pi);
 }
